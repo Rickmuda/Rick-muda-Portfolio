90f55cf51eeb9a1fbce558c6782dc0918b237d85
Rick <Rickambergen25@gmail.com>
2025-04-17 23:06:09 +0200

    1.2.2.9
    Added a new piece of art

--- src/components/windows/ArtGallery.vue
+++ src/components/windows/ArtGallery.vue
@@ -33,6 +33,7 @@ export default {
         new URL('@/assets/img/imggallery/vtuber.webp', import.meta.url).href,
         new URL('@/assets/img/imggallery/pose.webp', import.meta.url).href,
         new URL('@/assets/img/imggallery/dance.gif', import.meta.url).href,
+        new URL('@/assets/img/imggallery/rickpilgrim.webp', import.meta.url).href,
       ],
       selectedImageIndex: null, // Track the index of the selected image
     };
